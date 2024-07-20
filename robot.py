db995ae8045ce041c5896f41169f90d4366dd7a7
hydrol0x <mrjacobry@gmail.com>
2024-07-19 21:51:49 -0400

    initial robotpy project

---
+++ robot.py
@@ -0,0 +1,15 @@
+# TODO: insert robot code here
+import magicbot
+import wpilib
+
+class MyRobot(magicbot.MagicRobot):
+    x: int
+
+    def createObjects(self):
+        '''Create motors and stuff here'''
+        pass
+
+    def teleopInit(self):
+        '''Called when teleop starts; optional'''
+
+    def teleopPeriodic(self):
